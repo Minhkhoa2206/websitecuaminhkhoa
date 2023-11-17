c4c3b53e872ad319c33b0013c17b77e9609c011b
Minhkhoa2206 <97280663+Minhkhoa2206@users.noreply.github.com>
2023-11-17 11:00:50 +0700

    Add files via upload

--- dutoan.js
+++ dutoan.js
@@ -21,6 +21,5 @@ document.getElementById('button4').addEventListener('click', function() {
 });
 
 document.getElementById('button5').addEventListener('click', function() {
-    openLink('https://mega.nz/file/m74VibTb#FzW5JH1nPXIBc-IA6b_MprxVRupjQ6b_P5Ph62v3m-c');
+    openLink('https://drive.google.com/uc?export=download&id=1FP1Bv-WG3NgMgihzZ3Mxv4RNdH7OQHzO');
 });
-

c719950c6812ea6e47c3a60db16c91b649aa4587
Minhkhoa2206 <97280663+Minhkhoa2206@users.noreply.github.com>
2023-12-11 11:20:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-function openMenu() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
-  }
-
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-
-    document.getElementById('hours').innerText = hours;
-    document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
-
-    document.getElementById('hours').style.color = getRandomColor();
-    document.getElementById('minutes').style.color = getRandomColor();
-    document.getElementById('seconds').style.color = getRandomColor();
-}
-
-setInterval(updateClock, 1000);
-updateClock();
-document.firstElementChild.addEventListener("wheel", function(e) {
-  if (e.ctrlKey) {
-      e.preventDefault();
-  }
+// script.js
+document.getElementById('searchButton').addEventListener('click', function () {
+  search();
 });
 
+function search() {
+  var searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+  fetch('https://raw.githubusercontent.com/Minhkhoa2206/CH1/main/questions_and_answers.txt')
+    .then(response => response.text())
+    .then(data => {
+      var lines = data.split('\n');
+      var results = [];
+
+      for (var i = 0; i < lines.length; i++) {
+        if (lines[i].toLowerCase().includes(searchInput)) {
+          var result = lines.slice(i, i + 6);
+          results.push(result);
+        }
+      }
+
+      displayResults(results);
+    })
+    .catch(error => console.error('Lỗi khi tải nội dung:', error));
+}
+
+function displayResults(results) {
+  var searchResultsDiv = document.getElementById('searchResults');
+  searchResultsDiv.innerHTML = '';
+
+  if (results.length === 0) {
+    searchResultsDiv.innerHTML = 'Không có kết quả phù hợp.';
+  } else {
+    results.forEach(result => {
+      var resultDiv = document.createElement('div');
+      resultDiv.classList.add('result');
+
+      result.forEach(line => {
+        var resultParagraph = document.createElement('p');
+        resultParagraph.textContent = line;
+        resultDiv.appendChild(resultParagraph);
+      });
+
+      searchResultsDiv.appendChild(resultDiv);
+    });
+  }
+}

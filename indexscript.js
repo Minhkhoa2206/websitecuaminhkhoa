bc3357700d44840bde417916e74e38c03802fef6
Minh Khoa <97280663+Minhkhoa2206@users.noreply.github.com>
2024-03-07 20:42:55 +0700

    Upload indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,8 @@
+
+function openSourceCode() {
+  // Gán đường link vào biến
+  var sourceCodeLink = "https://by.tn/viewcode";
+
+  // Mở đường link trong một tab mới
+  window.open(sourceCodeLink, '_blank');
+}

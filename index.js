d13f1c652241687869d620493211ad21f8e4efef
Minh Khoa <97280663+Minhkhoa2206@users.noreply.github.com>
2024-03-07 20:42:47 +0700

    Upload index.js

---
+++ index.js
@@ -0,0 +1,277 @@
+// Define the data for HST MK elements
+const hstMkData = [
+    { 
+        name: "Kho dữ liệu phần mềm GDrive ( Chỉ danh nội bộ )",
+        image: "gdrive.png",
+        link: "https://drive.google.com/drive/folders/1du5EVZ5Gyyjf4DdrdfICtGqTN9udlh1W?usp=drive_link"
+    },
+    { 
+        name: "Software to convert Word file content language ( Dự án Python Developer)",
+        image: "evtovn.png",
+        link: "https://mega.nz/file/1u9ElQBL#TYvCLwsXb8gewAELQO-vshfHjjg8WK1OdiRc41q4KSE"
+    },
+    { 
+        name: "Software to convert EXCEL file content language ( Dự án Python Developer)",
+        image: "SEL.png",
+        link: "https://mega.nz/file/t6VFgYbS#jE8unUAoHSieJgBkVn0h5XqQqMSVqZ4y0mNLaz-FPvU"
+    },
+    { 
+        name: "Software to convert PowerPoint file content language ( Dự án Python Developer)",
+        image: "SPL.png",
+        link: "https://mega.nz/file/oiFWlTqA#eymcrRVvZnNiMvAbuKQohF82Y9UyT7wb-0eSsVl_j5I"
+    },
+    { 
+        name: "Detect Structural Cracks (Dự án Python Developer)",
+        image: "cracked.png",
+        link: "https://mega.nz/file/gy0lXCqB#1AdwvWBME3NY5w6rvAKrVt2Mc7qhxakxXoVhJGdfrSY"
+    },
+    { 
+        name: "Phần Mềm Tính Thể Tích  By Minh Khoa ( Dự án Python Developer)",
+        image: "pmthetich.png",
+        link: "https://drive.google.com/file/d/1nqFXsAaP833zRxlzXP1hyhc7TNcvkdeU/view?usp=sharing"
+    },
+    { 
+        name: "Tool Tối Ưu Windows By Minh Khoa | Version 1.0 | ( Dự án Python developer)",
+        image: "tooldn.png",
+        link: "https://drive.google.com/file/d/1nqFXsAaP833zRxlzXP1hyhc7TNcvkdeU/view?usp=sharing"
+    },
+    { 
+        name: "Tool Tối Ưu Windows By Minh Khoa | Version 2.0 | ( Dự án Python developer) NHIỀU TÍNH NẮNG MỚI SẤP ĐƯỢC UPDATE",
+        image: "tooldnv2.png",
+        link: "404.html"
+    },
+    { 
+        name: "Phần mềm Dowload Tube By Minh Khoa | Version 1.0 | ( Dự án Python Developer)",
+        image: "dowtube.png",
+        link: "https://drive.google.com/file/d/1D6pKZQ24MgagukbuTS-JsPfgWPgZSsCb/view?usp=sharing"
+    },
+    { 
+        name: "Tiện ích tra cứu câu hỏi trong bộ câu hỏi Chứng Chỉ Hành Nghề Xây Dựng ( Developer by Minh Khoa)",
+        image: "hnxd.png",
+        link: "https://drive.google.com/file/d/1-mXLC-t5fztURxsk0o9oTs-FfQFv9pDH/view?usp=drive_link"
+    },
+    { 
+        name: "Tool Fix Cảnh báo  + Xóa Key AutoDesk ( Dự án Python Developer)",
+        image: "fixbq.png",
+        link: "https://mega.nz/file/diEFXQgK#h_fF6A4ZQv19bS2VbWqAOuFpkc_TbtLyu-aDAXpWUos"
+    },
+    { 
+        name: "Plugin AutoSave AutoCAD |Version 1.0| ( Dự án Python Developer)",
+        image: "autosavecad.png",
+        link: "https://drive.google.com/file/d/1Gee1qwBPvvFB85qHS0eIhuGA4SMdEWUn/view?usp=sharing"
+    },
+    { 
+        name: "Plugin Đơn vị Chuẩn / AutoSave Sketchup",
+        image: "pluginsketchup.png",
+        link: "pluginsave.html"
+    }
+  ];
+  
+  // Define the data for useful software
+  const phammemtienichData = [
+      { 
+          name: "Phần mềm Văn phòng Microsoft Office",
+          image: "office1.png",
+          link: "office.html"
+      },
+      { 
+          name: "Active MAS Version 2.5",
+          image: "mas2.5.png",
+          link: "https://mega.nz/file/M7UgxDzT#xbKGfwu3pq99kDYI6lzr2Zxt4yVLqw8jT6mbo9zXkC4"
+      },
+      { 
+          name: "Phần mềm IDM (Full Active)",
+          image: "idm.jpg",
+          link: "https://mega.nz/file/Zi8zkKLI#5_qYbjc4DIOVFwPl_Hya0XJdN9Ry45tIRiX7r-cjCZg"
+      },
+      { 
+          name: "Kiểm tra SK ổ cứng CrystalDiskInfo",
+          image: "skoc.jpg",
+          link: "https://mega.nz/folder/nqIyVTaD#X_M5MyX8L_TJmVzZc3uVVA"
+      },
+      { 
+          name: "PM WinRaR Full Active",
+          image: "winrar.png",
+          link: "https://mega.nz/file/Ij9RnQ5J#jRvYtDlIgqgYb_z1EXE2n7D9uGZUCX-6MYJd7pdIzDg"
+      },
+      { 
+          name: "PM IObit Uninstaller 13 Pro (Full Key)",
+          image: "iob.jpg",
+          link: "https://mega.nz/file/QjURFAJT#TjAQ5ogSXs80nf_3GSHLv3cCSvvx6Bgn4_1Q_BblWvc"
+      },
+      { 
+          name: "Phần Mềm Tùy Chỉnh Bàn Phím KeyTweak V2.3.0",
+          image: "https://img.utdstc.com/screen/ceb/8e1/ceb8e19bcc55d120ab95d369a24ae37bbd64d3808d2d5c55277a61b68eed8547:600",
+          link: "https://mega.nz/file/YzNmUC7b#JVYozhkh4-iFKhcr7tHGWLLWY1iJqON1g7RMZc07GE8"
+      }
+  ];
+  
+  // Define the data for cloud storage services
+  const cloudStorageData = [
+      { 
+          name: "Google Drive ( Free 15GB)",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/640px-Google_Drive_icon_%282020%29.svg.png",
+          link: "https://drive.google.com/drive/home"
+      },
+      { 
+          name: "Dịch vụ lưu trữ MeGa ( Free 20GB)",
+          image: "https://www.ods.vn/wp-content/uploads/2022/08/mega-3.jpg",
+          link: "https://mega.nz/fm/1ykzBSyT"
+      },
+      { 
+          name: "Dịch vụ lưu trữ One Drive ( Free 5GB )",
+          image: "https://play-lh.googleusercontent.com/6AWr-qrhGT0ohjw0koq3bM8GHEFg1gTurald4FjCDg2RulTp4y_VVsYWUtw7Fo6lsQo",
+          link: "https://onedrive.live.com/login/"
+      }
+  ];
+  
+
+
+  
+  // Function to create HST MK elements and append to gallery
+  function createHSTMKElements() {
+    const hstMkGallery = document.getElementById('hst-mk-gallery');
+  
+    hstMkData.forEach(item => {
+        const galleryItem = document.createElement('div');
+        galleryItem.classList.add('gallery-item');
+  
+        const link = document.createElement('a');
+        link.href = item.link;
+        link.target = '_blank';
+  
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.alt = item.name;
+  
+        const caption = document.createElement('p');
+        caption.classList.add('image-caption');
+        caption.textContent = item.name;
+  
+        link.appendChild(image);
+        galleryItem.appendChild(link);
+        galleryItem.appendChild(caption);
+        hstMkGallery.appendChild(galleryItem);
+    });
+  }
+  
+  // Call the function to create HST MK elements
+  createHSTMKElements();
+  
+  // Define the selector for the custom element
+  const customSelector = '#custom-element';
+  
+  // Chọn phần tử sử dụng custom selector
+  const galleryContainer = document.querySelector(customSelector);
+  
+  phammemtienichData.forEach(item => {
+      const galleryItem = document.createElement('div');
+      galleryItem.classList.add('gallery-item');
+  
+      const link = document.createElement('a');
+      link.href = item.link;
+      link.target = '_blank';
+  
+      const image = document.createElement('img');
+      image.src = item.image;
+      image.alt = item.name;
+  
+      const caption = document.createElement('p');
+      caption.classList.add('image-caption');
+      caption.textContent = item.name;
+  
+      link.appendChild(image);
+      galleryItem.appendChild(link);
+      galleryItem.appendChild(caption);
+      galleryContainer.appendChild(galleryItem);
+  });
+  
+  // Select the container element for cloud storage
+  const cloudStorageContainer = document.getElementById('cloud-storage-gallery');
+  
+  // Create and append gallery items for cloud storage
+  cloudStorageData.forEach(item => {
+      const galleryItem = document.createElement('div');
+      galleryItem.classList.add('gallery-item');
+  
+      const link = document.createElement('a');
+      link.href = item.link;
+      link.target = '_blank';
+  
+      const image = document.createElement('img');
+      image.src = item.image;
+      image.alt = item.name;
+  
+      const caption = document.createElement('p');
+      caption.classList.add('image-caption');
+      caption.textContent = item.name;
+  
+      link.appendChild(image);
+      galleryItem.appendChild(link);
+      galleryItem.appendChild(caption);
+      cloudStorageContainer.appendChild(galleryItem);
+  });
+  
+
+
+  // Define the data for Windows installation tools
+const windowData = [
+    {
+        link: "https://drive.google.com/file/d/1x28drXUAOGhLqMnx3lJ02-xawa0LDN7d/view?usp=sharing",
+        imgSrc: "wintohdd.jpg",
+        caption: "Phần Mềm Cài Windows WintoHDD"
+    },
+    {
+        link: "https://drive.google.com/file/d/1x2rhhq-9GLu2y7Zv8N17PpQtACijUrkI/view?usp=sharing",
+        imgSrc: "rufus.png",
+        caption: "Phần mềm tạo USB boot Windows Rufus"
+    },
+    {
+        link: "https://www.microsoft.com/en-gb/software-download/windows10ISO",
+        imgSrc: "https://c.s-microsoft.com/en-gb/CMSImages/windows10-laptop.png?version=eec1eae8-03c5-1f2b-4160-3e2f5a7379d7",
+        caption: "Download ISO Windows 10 (Bản gốc Microsoft)"
+    },
+    {
+        link: "https://www.microsoft.com/software-download/windows11",
+        imgSrc: "https://c.s-microsoft.com/en-us/CMSImages/w11.png?version=211d8145-0900-cbb1-30dc-dd4498912d98",
+        caption: "Download ISO Windows 11 (Bản gốc Microsoft)"
+    }
+];
+
+// Select the container element for Windows installation tools
+const windowsContainer = document.getElementById('installwin');
+
+// Create and append gallery items for Windows installation tools
+windowData.forEach(item => {
+    // Create a div element for the gallery item
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery-item');
+
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = item.link;
+    link.target = '_blank';
+
+    // Create an image element
+    const image = document.createElement('img');
+    image.src = item.imgSrc;
+    image.alt = item.caption;
+
+    // Create a paragraph element for the caption
+    const caption = document.createElement('p');
+    caption.classList.add('image-caption');
+    caption.textContent = item.caption;
+
+    // Append the image to the link
+    link.appendChild(image);
+
+    // Append the link and caption to the gallery item
+    galleryItem.appendChild(link);
+    galleryItem.appendChild(caption);
+
+    // Append the gallery item to the container
+    windowsContainer.appendChild(galleryItem);
+});
+
+
+
